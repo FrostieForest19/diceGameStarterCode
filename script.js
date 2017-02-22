8e0967e65d9b3faa57fa09ee466e9380a9612630
Spencer Michalke <spencer.j.michalke@gmail.com>
2017-02-22 23:47:33 +0000

    rolldice

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   function rolldice(){
   //Place your code here 
-
-
-  };
-  
+  var dice = Math.random(Math.floor(1, 6));
+  var dice2 = Math.floor(dice + 1);
+ 
+  console.log(dice2);
+ }
+ $("#roll-dice").click(function){
 });
